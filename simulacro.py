7198def67484b766d06893485c8c3668e1d2eb40
JorgeRoblestx <jorge.robles.txurdinaga@gmail.com>
2023-09-27 08:43:53 +0200

    cambio jorge
    empezando

--- simulacro.py
+++ simulacro.py
@@ -1,3 +1,15 @@
 simulacrossss = "me quier0 m0rir"
 
 print (simulacrossss)
+
+salir = False
+
+crud = ("1 Create Persona \n" +
+        "2 Read Personas\n"
+        "3 Update Persona\n"
+        "4 Delete Persona\n"
+        "5 Exit\n")
+
+opt = input("Selecciona opción: ")
+
+print(crud)
